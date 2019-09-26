26c2e9f534c005429072d7d76a567b3cf714ea08
daler <dalerr@niddk.nih.gov>
2019-09-26 08:30:24 -0400

    documentation

--- setup.sh
+++ setup.sh
@@ -15,31 +15,41 @@ function showHelp() {
     echo "Options are intended to be run one-at-a-time; they are listed here in "
     echo "recommended order."
     echo
-    echo "  --apt-get-installs    (installs a bunch of useful Ubuntu packages)"
-    echo "  --docker              (installs docker and adds current user to new docker group)"
-    echo "  --download-miniconda  (downloads latest Miniconda to current directory)"
-    echo "  --install-miniconda   (install downloaded Miniconda to ~/miniconda3)"
-    echo "  --set-up-bioconda     (add channels for bioconda in proper order and make recommended speed-ups)"
-    echo "  --conda-env           (install requirements.txt into root conda env)"
-    echo "  --download-neovim-appimage (download appimage instead of compiling)"
-    echo "  --download-macos-nvim (download binary nvim for MacOS)"
-    echo "  --powerline           (installs powerline fonts)"
-    echo "  --set-up-nvim-plugins (manually add vim-plug)"
-    echo "  --nih-lablinux        (install repo for LabLinux and LabLinux itself)"
-    echo "  --set-up-lablinux     (print out recommended scripts to run from LabLinux)"
-    echo "  --centos7-installs    (compilers; recent tmux)"
-    echo "  --install-fzf         (installs fzf)"
-    echo "  --install-ag          (installs ag)"
-    echo "  --install-autojump    (installs autojump)"
-    echo "  --install-hub         (installs hub and sets alias)"
-    echo "  --install-fd          (installs fd and sets alias)"
-    echo "  --install-vd          (installs visidata and sets alias)"
-    echo "  --install-black       (installs black and sets alias)"
-    echo "  --install-tabview     (installs tabview and sets alias)"
-    echo "  --install-radian      (installs radian and sets alias)"
-    echo "  --alacritty           (installs alacritty, a GPU-accelerated terminal emulator)"
-    echo "  --diffs               (inspect differences between repo and home)"
-    echo "  --dotfiles            (update dotfiles)"
+    echo "Initial setup:"
+    echo -e "  ${GREEN} --apt-get-installs        [local only]  ${UNSET}(installs a bunch of useful Ubuntu packages)"
+    echo -e "  ${GREEN} --download-nvim-appimage  [local only]  ${UNSET}(download nvim AppImage)"
+    echo -e "  ${GREEN} --download-macos-nvim     [local only]  ${UNSET}(download binary nvim for MacOS)"
+    echo -e "  ${GREEN} --powerline               [local only]  ${UNSET}(installs powerline fonts)"
+    echo -e "  ${GREEN} --set-up-nvim-plugins                   ${UNSET}(manually add vim-plug)"
+    echo
+    echo "conda:"
+    echo -e "  ${GREEN} --download-miniconda                    ${UNSET}(downloads latest Miniconda to current directory)"
+    echo -e "  ${GREEN} --install-miniconda                     ${UNSET}(install downloaded Miniconda to ~/miniconda3)"
+    echo -e "  ${GREEN} --set-up-bioconda                       ${UNSET}(add channels for bioconda in proper order and make recommended speed-ups)"
+    echo -e "  ${GREEN} --conda-env                             ${UNSET}(install requirements.txt into root conda env)"
+    echo
+    echo "CentOS 7:"
+    echo -e "  ${GREEN} --nih-lablinux            [local only]  ${UNSET}(install repo for LabLinux and LabLinux itself)"
+    echo -e "  ${GREEN} --set-up-lablinux         [local only]  ${UNSET}(print out recommended scripts to run from LabLinux)"
+    echo -e "  ${GREEN} --centos7-installs        [local only]  ${UNSET}(compilers; recent tmux)"
+    echo
+    echo "Installations:"
+    echo -e "  ${GREEN} --install-fzf                           ${UNSET}(installs fzf)"
+    echo -e "  ${GREEN} --install-ag                            ${UNSET}(installs ag)"
+    echo -e "  ${GREEN} --install-autojump                      ${UNSET}(installs autojump)"
+    echo -e "  ${GREEN} --install-hub                           ${UNSET}(installs hub and sets alias)"
+    echo -e "  ${GREEN} --install-fd                            ${UNSET}(installs fd and sets alias)"
+    echo -e "  ${GREEN} --install-vd                            ${UNSET}(installs visidata and sets alias)"
+    echo -e "  ${GREEN} --install-black                         ${UNSET}(installs black and sets alias)"
+    echo -e "  ${GREEN} --install-tabview                       ${UNSET}(installs tabview and sets alias)"
+    echo -e "  ${GREEN} --install-radian                        ${UNSET}(installs radian and sets alias)"
+    echo -e "  ${GREEN} --install-git-cola                      ${UNSET}(installs git-cola and sets alias)"
+    echo -e "  ${GREEN} --install-docker          [local only]  ${UNSET}(installs docker and adds current user to new docker group)"
+    echo -e "  ${GREEN} --install-alacritty       [local only]  ${UNSET}(installs alacritty, a GPU-accelerated terminal emulator)"
+    echo
+    echo "Dotfiles:"
+    echo -e "  ${GREEN} --diffs                                   ${UNSET}(inspect differences between repo and home)"
+    echo -e "  ${GREEN} --dotfiles                                ${UNSET}(update dotfiles)"
     echo
     echo "paths to miniconda and neovim will be prepended to PATH in the"
     echo "~/.path file; that file will then be sourced"
@@ -110,6 +120,8 @@ check_for_conda () {
         exit 1
     fi
 }
+
+# Prompt user for info ($1 is text to provide)
 ok () {
     echo -e ${GREEN}$1${UNSET}
     read -p "Continue? (y/[n]) " -n 1 REPLY;
@@ -160,25 +172,25 @@ elif [ $task == "--download-miniconda" ]; then
     download https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh miniconda.sh
 
 elif [ $task == "--install-miniconda" ]; then
-    ok "Install Miniconda to $HOME/miniconda3/bin and then add $HOME/miniconda3/bin to the \$PATH by adding it to the end of ~/.path"
+    ok "Installs Miniconda to $HOME/miniconda3/bin and then add $HOME/miniconda3/bin to the \$PATH by adding it to the end of ~/.path"
     bash miniconda.sh -b
     echo "# Added `date`:"
     echo "export PATH=\"\$PATH:$HOME/miniconda3/bin\"" >> ~/.path
     source ~/.path
 
 elif [ $task == "--set-up-bioconda" ]; then
-    ok "Set up Bioconda by adding the dependent channels in the correct order"
+    ok "Sets up Bioconda by adding the dependent channels in the correct order"
     conda config --add channels defaults
     conda config --add channels bioconda
     conda config --add channels conda-forge
 
 elif [ $task == "--conda-env" ]; then
-    ok "Install dependencies in 'requirements.txt' into the base conda environment"
+    ok "Installs dependencies in 'requirements.txt' into the base conda environment"
     conda install --file requirements.txt
 
 
 elif [ $task == "--powerline" ]; then
-    ok "Install patched powerline fonts from https://github.com/powerline/fonts"
+    ok "Installs patched powerline fonts from https://github.com/powerline/fonts"
     git clone https://github.com/powerline/fonts.git --depth 1 /tmp/fonts
     (cd /tmp/fonts && ./install.sh)
     rm -rf /tmp/fonts
@@ -186,8 +198,8 @@ elif [ $task == "--powerline" ]; then
     echo "Change your terminal's config to use the new powerline patched fonts"
     echo
 
-elif [ $task == "--download-neovim-appimage" ]; then
-    ok "Download AppImage for neovim, install into $HOME/opt/neovim/bin/nvim, and add that to the \$PATH via the ~/.path file"
+elif [ $task == "--download-nvim-appimage" ]; then
+    ok "Downloads AppImage for neovim, install into $HOME/opt/neovim/bin/nvim, and add that to the \$PATH via the ~/.path file"
     dest="$HOME/opt/neovim/bin/nvim"
     mkdir -p $(dirname $dest)
     download https://github.com/neovim/neovim/releases/download/v0.3.4/nvim.appimage $dest
@@ -196,7 +208,7 @@ elif [ $task == "--download-neovim-appimage" ]; then
     source ~/.path
 
 elif [ $task == "--download-macos-nvim" ]; then
-    ok "Download neovim tarball from https://github.com/neovim/neovim, install
+    ok "Downloads neovim tarball from https://github.com/neovim/neovim, install
     into $HOME/opt/neovim, and add that to the \$PATH via the ~/.path file"
     download https://github.com/neovim/neovim/releases/download/v0.3.4/nvim-macos.tar.gz nvim-macos.tar.gz
     tar -xzvf nvim-macos.tar.gz
@@ -206,7 +218,7 @@ elif [ $task == "--download-macos-nvim" ]; then
     source ~/.path
 
 elif [ $task == "--set-up-nvim-plugins" ]; then
-    ok "Download plug.vim into ~/.local/share/nvim/site/autoload/plug.vim"
+    ok "Downloads plug.vim into ~/.local/share/nvim/site/autoload/plug.vim"
     dest=~/.local/share/nvim/site/autoload/plug.vim
     mkdir -p $(dirname $dest)
     download https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim $dest
@@ -215,13 +227,13 @@ elif [ $task == "--set-up-nvim-plugins" ]; then
     echo
 
 elif [ $task == "--diffs" ]; then
-    ok "Show the diffs between this repo and what's in your home directory"
+    ok "Shows the diffs between this repo and what's in your home directory"
     cmd="diff --recursive --exclude .git --exclude setup.sh --exclude README.md --exclude Miniconda3-latest-Linux-x86_64.sh --exclude LICENSE-MIT.txt"
     $cmd ~ . | grep -v "Only in $HOME" | sed "s|$cmd||g"
 
 
 elif [ $task == "--nih-lablinux" ]; then
-    ok "Assumes CentOS 7. Install lablinux repo, but don't run any commands"
+    ok "Assumes CentOS 7. Installs lablinux repo, but don't run any commands"
     set -ex
     sudo yum install redhat-lsb-core
     sudo yum  install \
@@ -243,7 +255,7 @@ elif [ $task == "--set-up-lablinux" ]; then
     echo
 
 elif [ $task == "--centos7-installs" ]; then
-    ok "Install packages on CentOS 7 (includes compiling a recent-ish tmux)"
+    ok "Installs packages on CentOS 7 (includes compiling a recent-ish tmux)"
     set -ex
     sudo yum install epel-release
     sudo yum groupinstall "GNOME Desktop"
@@ -271,14 +283,14 @@ elif [ $task == "--centos7-installs" ]; then
     set +ex
 
 elif [ $task == "--install-fzf" ]; then
-    ok "Install fzf (https://github.com/junegunn/fzf)"
+    ok "Installs fzf (https://github.com/junegunn/fzf)"
     (
       git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
       ~/.fzf/install --no-update-rc --completion --key-bindings
       )
 
 elif [ $task == "--install-ag" ]; then
-    ok "Install ag into $HOME/opt (https://github.com/ggreer/the_silver_searcher)"
+    ok "Installs ag into $HOME/opt (https://github.com/ggreer/the_silver_searcher)"
     (
         agdir=$HOME/tmp/ag
         rm -rf $agdir
@@ -292,7 +304,7 @@ elif [ $task == "--install-ag" ]; then
     )
 
 elif [ $task == "--install-autojump" ]; then
-    ok "Install autojump (https://github.com/wting/autojump)"
+    ok "Installs autojump (https://github.com/wting/autojump)"
     (
         git clone https://github.com/wting/autojump.git
         cd autojump
@@ -301,7 +313,7 @@ elif [ $task == "--install-autojump" ]; then
     rm -rf autojump
 
 elif [ $task == "--install-hub" ]; then
-    ok "Install hub to $HOME/opt (https://github.com/github/hub)"
+    ok "Installs hub to $HOME/opt (https://github.com/github/hub)"
     HUB_VERSION=2.11.2
     (
         download https://github.com/github/hub/releases/download/v${HUB_VERSION}/hub-linux-amd64-${HUB_VERSION}.tgz /tmp/hub.tar.gz
@@ -376,8 +388,8 @@ elif [ $task == "--dotfiles" ]; then
     fi
     unset doIt
 
-elif [ $task == "--alacritty" ]; then
-    ok "Install alacritty terminal. Also needs to install rust"
+elif [ $task == "--install-alacritty" ]; then
+    ok "Installs alacritty terminal. Also needs to install rust"
     (
         set -eoux
         SRC=/tmp/alacritty
