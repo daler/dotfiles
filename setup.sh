99e7f1836be39c539d3eee8641ce1afe0855e5d3
daler <dalerr@niddk.nih.gov>
2019-09-27 14:40:36 -0400

    --install-bat

--- setup.sh
+++ setup.sh
@@ -44,6 +44,7 @@ function showHelp() {
     echo -e "  ${GREEN} --install-tabview                       ${UNSET}(installs tabview and sets alias)"
     echo -e "  ${GREEN} --install-radian                        ${UNSET}(installs radian and sets alias)"
     echo -e "  ${GREEN} --install-git-cola                      ${UNSET}(installs git-cola and sets alias)"
+    echo -e "  ${GREEN} --install-bat                           ${UNSET}(installs bat and sets alias)"
     echo -e "  ${GREEN} --install-docker          [local only]  ${UNSET}(installs docker and adds current user to new docker group)"
     echo -e "  ${GREEN} --install-alacritty       [local only]  ${UNSET}(installs alacritty, a GPU-accelerated terminal emulator)"
     echo
@@ -375,6 +376,17 @@ elif [ $task == "--install-git-cola" ]; then
     git clone git://github.com/git-cola/git-cola.git ~/opt/git-cola
     add_line_to_file "alias git-cola=\"$CONDA_LOCATION/envs/git-cola/bin/python $HOME/opt/git-cola/bin/git-cola\"" ~/.aliases
     remind_alias
+
+
+elif [ $task == "--install-bat" ]; then
+    ok "Installs bat (https://github.com/sharkdp/bat). Extracts the binary to ~/opt/bin"
+    BAT_VERSION=0.12.1
+    download "https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat-v${BAT_VERSION}-x86_64-unknown-linux-musl.tar.gz" "/tmp/bat-${BAT_VERSION}.tar.gz"
+    mkdir -p /tmp/bat
+    tar -xf "/tmp/bat-${BAT_VERSION}.tar.gz" -C /tmp/bat/
+    mkdir -p ~/opt/bin
+    cp /tmp/bat/bat*/bat ~/opt/bin
+    rm -r /tmp/bat "/tmp/bat-${BAT_VERSION}.tar.gz"
 
 elif [ $task == "--dotfiles" ]; then
     ok "Copies over all the dotfiles here to your home directory. Prompts again before actually running to make sure!"
