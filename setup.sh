b7c606be3c395a6af6078c9ac1a0c64bb25732d3
Ryan Dale <ryan.dale@nih.gov>
2019-11-30 21:15:43 -0500

    update nvim appimage version

--- setup.sh
+++ setup.sh
@@ -229,7 +229,7 @@ elif [ $task == "--download-nvim-appimage" ]; then
     ok "Downloads AppImage for neovim, install into $HOME/opt/neovim/bin/nvim, and add that to the \$PATH via the ~/.path file"
     dest="$HOME/opt/neovim/bin/nvim"
     mkdir -p $(dirname $dest)
-    download https://github.com/neovim/neovim/releases/download/v0.3.4/nvim.appimage $dest
+    download https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage $dest
     chmod u+x $dest
     echo "export PATH=\"$HOME/opt/neovim/bin:\$PATH\"" >> ~/.path
     source ~/.path
