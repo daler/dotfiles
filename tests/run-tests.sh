fae75cb840ac57bcbe91e91854b4156848985fae
Ryan Dale <ryan.dale@nih.gov>
2023-09-18 11:09:46 -0400

    [minor] change where IFS is reset

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#
 FAILURES=0
 GREEN="\e[32m"
 YELLOW="\e[33m"
 RED="\e[31m"
 BLUE="\e[34m"
 UNSET="\e[0m"
+
 OLDIFS=$IFS; IFS=$'\n';
 while read line; do
     tst=$(echo $line | awk -F "\t" '{print $1}')
@@ -18,8 +20,10 @@ while read line; do
 
     fi
 done < test_commands
+
+IFS=OLDIFS
+
 if [[ $FAILURES == 1 ]]; then
     printf "${RED}Failures found, see above.${UNSET}\n\n"
     exit 1
 fi
-IFS=OLDIFS
