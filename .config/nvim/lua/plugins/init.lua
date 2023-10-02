6376203666acca336cb9ee9ee5e62587daf38231
daler <dalerr@niddk.nih.gov>
2023-10-02 19:55:49 -0400

    move lazy.nvim bootstrap to plugins dir

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,3 +1,20 @@
+-- Bootstrap lazy.nvim in case it's not already installed.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Return the plugin spec. Note that plugins with additional config are placed
+-- in their own files in the plugins/ dir.
+
 return {
   {'vim-pandoc/vim-pandoc-syntax'},
   {'vim-pandoc/vim-rmarkdown'},
