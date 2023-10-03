a3629c4ed546049308bf4a243876f48d912485df
daler <dalerr@niddk.nih.gov>
2023-10-02 20:32:50 -0400

    rm separate lazy boostrap command

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,6 @@
 -- lua/ directory.
 
 require("settings")               -- See lua/settings.lua
-require("lazy-bootstrap")         -- Ensure lazy.nvim is installed
 require("lazy").setup("plugins")  -- lazily-load plugins. See lua/plugins/init.lua
 require("mappings")               -- See lua/mappings.lua
 require("autocommands")           -- See lua/autocommands.lua
