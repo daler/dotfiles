e9b8260024fe75fd89cacc2dc26d5079b49b7992
daler <dalerr@niddk.nih.gov>
2023-10-02 22:12:22 -0400

    minor

--- .config/nvim/lua/plugins/vim-pandoc.lua
+++ .config/nvim/lua/plugins/vim-pandoc.lua
@@ -1,6 +1,6 @@
 return  {
     'vim-pandoc/vim-pandoc',
-    config = function()
+    init = function()
       vim.cmd("let g:pandoc#syntax#conceal#use = 0") -- " Disable the conversion of ``` to lambda and other fancy concealment/conversion that ends up confusing me
       vim.cmd("let g:pandoc#folding#fold_fenced_codeblocks = 1") -- " RMarkdown code blocks can be folded too
       vim.cmd("let g:pandoc#spell#enabled = 0") -- " By default, keep spell-check off. Turn on with `set spell`
