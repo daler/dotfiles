dac2d5b708c10978f93e91fef94011ebf066e7be
daler <dalerr@niddk.nih.gov>
2023-10-02 20:36:08 -0400

    back to lazy bootstrap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@
 -- lua/ directory.
 
 require("settings")               -- See lua/settings.lua
+require("lazy-bootstrap")         -- See lua/lazy-bootstrap.lua
 require("lazy").setup("plugins")  -- lazily-load plugins. See lua/plugins/init.lua
 require("mappings")               -- See lua/mappings.lua
 require("autocommands")           -- See lua/autocommands.lua
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,17 +1,3 @@
--- Bootstrap lazy.nvim in case it's not already installed.
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- Return the plugin spec. Note that plugins with additional config are placed
 -- in their own files in the plugins/ dir.
 
