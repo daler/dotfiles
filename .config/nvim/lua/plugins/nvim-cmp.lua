08ee7b20828dc6044fbf8a1706feb4cfcd1b6c95
daler <dalerr@niddk.nih.gov>
2023-10-02 20:05:46 -0400

    use luasnip config for nvim-cmp

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -9,62 +9,63 @@ return {
       "saadparwaiz1/cmp_luasnip",
       "L3MON4D3/LuaSnip",
     },
+    -- The following is from the nvim-cmp wiki, which shows how to configure
+    -- similar to SuperTab. That is, the window only appears when you hit tab,
+    -- and replaces the text you're writing.
     config = function()
-
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      local feedkey = function(key, mode)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-      end
-
+      local luasnip = require("luasnip")
       local cmp = require('cmp')
 
       cmp.setup {
         -- even if you don't use snippets, nvim-cmp needs a snippets plugin configured.
         snippet = {
           expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
+            require('luasnip').lsp_expand(args.body)
           end,
         },
 
-        -- The following is from the nvim-cmp wiki, which shows how to configure similar to SuperTab.
-        -- That is, the window only appears when you hit tab, and replaces the text you're writing.
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-              feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+      mapping = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+        -- they way you will only jump inside the snippet region
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
 
-          ["<S-Tab>"] = cmp.mapping(function()
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-              feedkey("<Plug>(vsnip-jump-prev)", "")
-            end
-          end, { "i", "s" }),
-        }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      },
 
-        -- Prevent the window from appearing unless you hit TAB.
-        completion = {
-          autocomplete = false,
-        },
+      -- Prevent the window from appearing unless you hit TAB.
+      completion = {
+        autocomplete = false,
+      },
 
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'buffer' },
-          { name = 'path' },
-        }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+      }),
       }
     end,
 }
