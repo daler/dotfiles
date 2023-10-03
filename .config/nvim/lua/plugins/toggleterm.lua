907b9710ac1085a9113decd9fa3e5a2099e66584
daler <dalerr@niddk.nih.gov>
2023-10-02 22:11:59 -0400

    move toggleterm mappings to toggleterm config

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -14,6 +14,30 @@ return  {
       end
     }
 
+    vim.keymap.set('n', 'gxx', ':ToggleTermSendCurrentLine<CR><CR>',
+      { desc = "Send current line to terminal" })
+
+    vim.keymap.set('x', 'gx', ':ToggleTermSendVisualSelection<CR><CR>',
+      { desc = "Send selection to terminal" })
+
+    -- " <leader>cd sends RMarkdown code chunk and move to the next one.
+    -- "
+    -- " Breaking this down...
+    -- "
+    -- " /```{<CR>                                 -> search for chunk delimiter (recall <CR> is Enter)
+    -- " N                                         -> find the *previous* match to ```{
+    -- " j                                         -> move down one line from the previous match
+    -- " V                                         -> enter visual line-select mode
+    -- " /^```\n<CR>                               -> select until the next chunk delimiter by itself on the line (which should be the end)
+    -- " k                                         -> go up one line from that match so we don't include that line
+    -- " <Esc>:ToggleTermSendVisualSelection<CR>   -> send the selection to the terminal
+    -- " /```{r<CR>                                -> go to the start of the next chunk
+    vim.keymap.set('n', '<leader>cd', '/```{<CR>NjV/```\n<CR>k<Esc>:ToggleTermSendVisualSelection<CR>/```{r<CR>',
+      { desc = "Send RMarkdown chunk to R"})
+
+    vim.keymap.set('n', '<leader>t', ':ToggleTerm direction=vertical<CR>',
+      { desc = "New terminal on right"})
+
     -- Render RMarkdown to HTML
     vim.api.nvim_create_autocmd('FileType', { pattern = "rmarkdown",
     callback = function()
