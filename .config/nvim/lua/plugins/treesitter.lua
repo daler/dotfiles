963bba723d11839a502c62b12776d76013feb567
Ryan Dale <ryan.dale@nih.gov>
2023-10-06 15:50:56 -0400

    rm textsubjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,9 +3,6 @@ return {
   version = false,
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
   config = function()
     require("nvim-treesitter.configs").setup({
       highlight = { enable = true },
@@ -33,16 +30,6 @@ return {
           node_incremental = "<C-space>",
           scope_incremental = false,
           node_decremental = "<BS>",
-        },
-      },
-      textsubjects = {
-        enable = true,
-        prev_selection = ",", -- (Optional) keymap to select the previous selection
-        keymaps = {
-          ["."] = "textsubjects-smart",
-          [";"] = "textsubjects-container-outer",
-          ["i;"] = "textsubjects-container-inner",
-          ["i;"] = { "textsubjects-container-inner", desc = "Select inside containers (classes, functions, etc.)" },
         },
       },
     })
