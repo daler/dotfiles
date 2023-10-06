53fefd97ee34e2b910575dfbde9a6388057af480
Ryan Dale <ryan.dale@nih.gov>
2023-10-06 14:05:11 -0400

    zenburn tweaks

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -2,25 +2,32 @@ require("zenburn")
 vim.cmd("set termguicolors")
 vim.cmd("colorscheme zenburn")
 
---
--- The following are some tweaks to the zenburn colorscheme.
---
--- Some hints:
+-- Some hints for tweaking colorschemes:
 --    Use :Inspect to see what highlight group the cursor is over.
 --    Use :Telescope higlight to search for it and find the current setting
 
--- Make the line numbers a little more obvious than the default.
-vim.cmd("highlight LineNr guifg=#959898 guibg=#353535")
-vim.cmd("highlight CursorLineNr guifg=#f2f48d guibg=#2f2f2f")
+if vim.api.nvim_exec("colorscheme", true) == "zenburn.nvim" then
+  -- The following are some tweaks to the zenburn colorscheme.
 
--- Use the original zenburn's incremental search highlight
-vim.cmd("highlight IncSearch guifg=#f8f893 guibg=#385f38")
+  -- Make the line numbers a little more obvious than the default.
+  vim.cmd("highlight LineNr guifg=#959898 guibg=#353535")
+  vim.cmd("highlight CursorLineNr guifg=#f2f48d guibg=#2f2f2f")
 
--- Use italics for comments 
-vim.cmd("highlight Comment cterm=italic gui=italic")
+  -- Use the original zenburn's incremental search highlight
+  vim.cmd("highlight IncSearch guifg=#f8f893 guibg=#385f38")
 
--- Brighter diff delete, for better visibility in gitsigns
-vim.cmd("highlight DiffDelete guifg=#9f8888 guibg=#464646")
+  -- Use italics for comments (may be difficult to get working under tmux)
+  vim.cmd("highlight Comment cterm=italic gui=italic")
 
--- seems to over-highlight identifiers, so set to plain white
-vim.cmd("highlight Identifier guifg=#dcdccc")
+  -- Brighter diff delete, for better visibility in gitsigns
+  vim.cmd("highlight DiffDelete guifg=#9f8888 guibg=#464646")
+
+  vim.cmd("highlight @variable.python guifg=#dcdccc")
+  vim.cmd("highlight @constant.python guifg=#dcdccc gui=bold")
+  vim.cmd("highlight @boolean.python guifg=#FFCFAF gui=bold")
+  vim.cmd("highlight Function guifg=#f4f4c2")
+
+  --
+  vim.cmd("highlight IblScope guifg=#efefaf")
+  vim.cmd("highlight @ibl.scope.char.1 guifg=#efefaf")
+end
