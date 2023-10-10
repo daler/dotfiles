e66d740bc0a729ded3364616b84aa9cf38a9d990
Ryan Dale <ryan.dale@nih.gov>
2023-10-10 17:58:48 -0400

    add one-liner plugin descriptions

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -9,33 +9,34 @@
 -- along with the description these are picked up by which-key.
 --
 return {
-  { "vim-scripts/vis" },
-  { "chrisbra/vim-diff-enhanced" },
-  { "roxma/vim-tmux-clipboard" },
-  { "dhruvasagar/vim-table-mode" },
-  { "tpope/vim-commentary" },
-  { "tpope/vim-surround" },
-  { "tpope/vim-sleuth" },
-  { "Vimjas/vim-python-pep8-indent", ft = { "python", "snakemake" } },
-  { "samoshkin/vim-mergetool", cmd = "MergetoolStart" },
-  { "tpope/vim-fugitive", cmd = "Git", lazy = true },
-  { "sindrets/diffview.nvim", cmd = { "DiffviewOpen", "DiffviewFileHistory" } },
-  { "stsewd/sphinx.nvim", ft = "rst" },
+  { "vim-scripts/vis" }, -- restrict replacement only to visual selection
+  { "chrisbra/vim-diff-enhanced" }, -- provides other diff algorithms
+  { "roxma/vim-tmux-clipboard" }, -- clipboard works better with tmux
+  { "dhruvasagar/vim-table-mode" }, -- easily work with ReST and Markdown tables
+  { "tpope/vim-commentary" }, -- toggle comments
+  { "tpope/vim-surround" }, -- text objects for surrounding characters
+  { "tpope/vim-sleuth" }, -- autodetect indentation settings
+  { "Vimjas/vim-python-pep8-indent", ft = { "python", "snakemake" } }, -- better indentation for python
+  { "samoshkin/vim-mergetool", cmd = "MergetoolStart" }, -- easily work with 3-way merge conflicts
+  { "tpope/vim-fugitive", cmd = "Git", lazy = true }, -- convenient git interface, with incremental commits
+  { "sindrets/diffview.nvim", cmd = { "DiffviewOpen", "DiffviewFileHistory" } }, -- nice diff interface
+  { "stsewd/sphinx.nvim", ft = "rst" }, -- syntax/grammar for Sphinx (ReST) documentation
+  { "folke/which-key.nvim", lazy = false, config = true }, -- pop up a window showing possible keybindings
+  { "phha/zenburn.nvim", lazy = false, priority = 1000 }, -- colorscheme
+  { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 }, 
+  { "morhetz/gruvbox", enabled = false }, -- example of an alternative colorscheme, here disabled
   {
-    "vim-pandoc/vim-pandoc-syntax",
+    "vim-pandoc/vim-pandoc-syntax", -- improves editing markdown and ReST
     ft = { "markdown", "rmarkdown" },
     dependencies = { "vim-pandoc/vim-pandoc" }
   },
   {
-    "vim-pandoc/vim-rmarkdown",
+    "vim-pandoc/vim-rmarkdown", -- improves editing RMarkdown
     ft = "rmarkdown",
     dependencies = { "vim-pandoc/vim-pandoc-syntax", "vim-pandoc/vim-pandoc" },
   },
-  { "folke/which-key.nvim", lazy = false, config = true },
-  { "phha/zenburn.nvim", lazy = false, priority = 1000 },
-  { "morhetz/gruvbox", enabled = false }, -- example of an alternative colorscheme, here disabled
   {
-    "nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope.nvim", -- pop-up window used for fuzzy-searching and selecting
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
@@ -56,21 +57,18 @@ return {
     },
   },
   {
-    "danilamihailov/beacon.nvim",
+    "danilamihailov/beacon.nvim", -- flash a beacon to show where you are
     lazy = false, -- otherwise, on first KJ you get a double-flash
     keys = {
       { "<S-k><S-j>", ":Beacon<CR>", desc = "Flash beacon" },
-      { "N", "N:Beacon<CR>" }, -- prev search flashes beacon
-      { "n", "n:Beacon<CR>" }, -- next search flashes beacon
-      { "%", "%:Beacon<CR>" }, -- matching bracket flashes beacon
-      { "*", "*:Beacon<CR>" }, -- next word under cursor flashes beacon
+      { "N", "N:Beacon<CR>", desc = "Prev search hit and flash beacon"},
+      { "n", "n:Beacon<CR>", desc = "Next search hit and flash beacon" },
+      { "%", "%:Beacon<CR>", desc = "Go to matching bracket and flash beacon" },
+      { "*", "*:Beacon<CR>" , desc = "Go to next word occurrence and flash beacon" },
     },
-    config = function()
-      vim.cmd("let g:beacon_show_jumps=0") -- default only flashes if jumping >10 lines
-    end,
   },
   {
-    "rhysd/accelerated-jk",
+    "rhysd/accelerated-jk", -- speeds up vertical navigation
     keys = {
       { "j", "<Plug>(accelerated_jk_gj)" },
       { "k", "<Plug>(accelerated_jk_gk)" },
@@ -81,7 +79,7 @@ return {
     end,
   },
   {
-    "vim-pandoc/vim-pandoc",
+    "vim-pandoc/vim-pandoc", -- improves editing markdown and RMarkdown
     lazy = true,
     init = function()
       vim.cmd("let g:pandoc#syntax#conceal#use = 0") -- " Disable the conversion of ``` to lambda and other fancy concealment/conversion that ends up confusing me
@@ -91,7 +89,7 @@ return {
     end,
   },
   {
-    "akinsho/toggleterm.nvim",
+    "akinsho/toggleterm.nvim", -- terminal in vim you can send code to
     config = function()
       require("toggleterm").setup({
         size = function(term)
@@ -131,8 +129,9 @@ return {
       { "<leader>t", ":ToggleTerm direction=vertical<CR>", desc = "New terminal on right" },
     },
   },
+
   {
-    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter", -- sophisticated syntax highlighting and code inspection
     version = false,
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
@@ -176,13 +175,13 @@ return {
     end,
   },
   {
-    "ggandor/leap.nvim",
+    "ggandor/leap.nvim", -- quickly jump around the buffer without counting lines
     config = function()
       require("leap").set_default_keymaps()
     end,
   },
   {
-    "lukas-reineke/indent-blankline.nvim",
+    "lukas-reineke/indent-blankline.nvim", -- show vertical lines at tabstops
     lazy = false,
     main = "ibl",
     opts = {
@@ -191,7 +190,7 @@ return {
     },
   },
   {
-    "stevearc/aerial.nvim",
+    "stevearc/aerial.nvim", -- code navigator
     dependencies = {
       "nvim-tree/nvim-web-devicons",
       "nvim-treesitter/nvim-treesitter",
@@ -204,7 +203,7 @@ return {
     },
   },
   {
-    "lewis6991/gitsigns.nvim",
+    "lewis6991/gitsigns.nvim", -- show changes in git repo
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
@@ -236,7 +235,7 @@ return {
     },
   },
   {
-    "vim-airline/vim-airline",
+    "vim-airline/vim-airline", -- status line along the bottom
     dependencies = { "vim-airline/vim-airline-themes" },
     init = function()
       vim.cmd("let g:airline_theme='ayu_dark'") -- theme that looks good with zenburn
@@ -248,7 +247,7 @@ return {
     end,
   },
   {
-    "L3MON4D3/LuaSnip",
+    "L3MON4D3/LuaSnip", -- allow tab completion of snippets
     lazy = true,
     dependencies = {
       "rafamadriz/friendly-snippets",
@@ -283,7 +282,7 @@ return {
     },
   },
   {
-    "hrsh7th/nvim-cmp",
+    "hrsh7th/nvim-cmp", -- tab completion
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
