2d33580e0d8e09056841667a0f412024fe6af4dc
Ryan Dale <ryan.dale@nih.gov>
2023-11-07 10:16:41 -0500

    explicity specify highlight-on-yank params for better discoverability

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -168,7 +168,7 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank{higroup = "IncSearch", timeout=100}
   end,
   pattern = "*",
 })
