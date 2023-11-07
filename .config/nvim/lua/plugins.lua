dc1f7d2b7fa582edc8b2d3c7862490c59f11d8c0
Ryan Dale <ryan.dale@nih.gov>
2023-11-07 10:16:10 -0500

    disable the beacon globally, only use for search and JK

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -87,6 +87,10 @@ return {
   {
     "danilamihailov/beacon.nvim", -- flash a beacon to show where you are
     lazy = false, -- otherwise, on first KJ you get a double-flash
+    config = function ()
+      -- Disable the beacon globally; only the commands below will activate it.
+      vim.cmd("let g:beacon_enable=0")
+    end,
     keys = {
       { "<S-k><S-j>", ":Beacon<CR>", desc = "Flash beacon" },
       { "N", "N:Beacon<CR>", desc = "Prev search hit and flash beacon" },
