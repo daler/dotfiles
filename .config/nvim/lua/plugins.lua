b63c23684ab342c056dcc428a6785d005a6f165d
Ryan Dale <ryan.dale@nih.gov>
2024-04-27 11:01:05 -0400

    add stickybuf plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -522,5 +522,17 @@ return {
     },
   },
 
+  {
+    "stevearc/stickybuf.nvim", -- Prevents opening buffers in terminal buffer
+    opts = {},
+    config = function()
+      require("stickybuf").setup({
+        get_auto_pin = function(bufnr)
+          return require("stickybuf").should_auto_pin(bufnr)
+        end,
+      })
+    end,
+  },
+
 }
 -- vim: nowrap
