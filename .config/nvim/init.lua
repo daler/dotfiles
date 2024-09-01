1c9be0b275d0daaa99852ee074501fd110463190
Ryan Dale <ryan.dale@nih.gov>
2024-08-31 21:17:22 -0400

    improve <leader>cp
    now also toggles render-markdown plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,11 +111,21 @@ vim.keymap.set("n", "H", ":bprevious<CR>", { desc = "Previous buffer" })
 vim.keymap.set("n", "L", ":bnext<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>cp",
   function()
+    -- Turn off various symbols and text that shouldn't be copied, such as
+    -- indent-blankline vertical lines, indicators of git changes, and various
+    -- virtual text and symbols.
+    --
+    -- Also toggles line numbers.
+    --
+    -- In all cases, only disable if the plugin is loaded in the first place.
     if package.loaded["ibl"] ~= nil then
       vim.cmd("IBLToggle")
     end
     if package.loaded["gitsigns"] ~= nil then
       vim.cmd("Gitsigns toggle_signs")
+    end
+    if package.loaded["render-markdown"] ~= nil then
+      vim.cmd(":RenderMarkdown toggle")
     end
     vim.cmd("set nu!")
   end,
