d0ade8879f4d8174db93f24209fd047451a53289
Brandon Fuller <brandonfuller007@gmail.com>
2024-10-15 11:19:40 -0400

    Add check if env prompt line exists already
    Add check if `env_prompt:` line already exists in ~/.condarc and if it
    does not then setup.sh will add the line

--- setup.sh
+++ setup.sh
@@ -482,7 +482,9 @@ elif [ $task == "--set-up-bioconda" ]; then
     conda config --add channels bioconda
     conda config --add channels conda-forge
     conda config --set channel_priority strict
-    echo "env_prompt: '({name}) '" >> ~/.condarc
+    if ! grep -q "env_prompt:" ~/.condarc; then
+        echo "env_prompt: '({name}) '" >> ~/.condarc
+    fi
     printf "${YELLOW}Channels configured, see ~/.condarc${UNSET}\n"
 
 
