b8846ab652619e8699a90b506acb2863e12f5646
Ryan Dale <115406+daler@users.noreply.github.com>
2025-08-04 09:09:08 -0400

    change url format for nvim release

--- setup.sh
+++ setup.sh
@@ -519,7 +519,7 @@ elif [ $task == "--install-neovim" ]; then
         mkdir -p "$HOME/opt/bin"
         mv nvim-macos-*64 "$HOME/opt/neovim"
     else
-        download https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux64.tar.gz nvim-linux64.tar.gz
+        download https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim-linux-x86_64.tar.gz nvim-linux64.tar.gz
         tar -xzf nvim-linux64.tar.gz
         mv nvim-linux64 "$HOME/opt/neovim"
         rm nvim-linux64.tar.gz
