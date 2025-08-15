3deb34413d1e2b58ce082c6d5edd289ec10fafc8
Ryan Dale <115406+daler@users.noreply.github.com>
2025-08-14 22:02:47 -0400

    add treesj

---
+++ .config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,12 @@
+-- treesj splits and joins nodes
+return {
+  'Wansmer/treesj',
+  lazy = false,
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesj').setup({
+      use_default_keymaps = false,
+    })
+    vim.keymap.set('n', '<leader>j', require('treesj').toggle, { desc = "Toggle split/join nodes" })
+  end,
+}
